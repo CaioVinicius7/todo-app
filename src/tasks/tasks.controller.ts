f8ddfeb6a310cf59f0d3564f4e0ee6f50147db71
Caio Vinícius <caio1525pereira@gmail.com>
2022-10-28 01:28:47 -0300

    feat: :sparkles: CRUD

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post
+} from "@nestjs/common";
+
+import { Task } from "./tasks";
+import { TasksService } from "./tasks.service";
 
 @Controller("tasks")
-export class TasksController {}
+export class TasksController {
+  constructor(private taskService: TasksService) {}
+
+  @Get()
+  async getAll(): Promise<Task[]> {
+    return this.taskService.getAll();
+  }
+
+  @Get(":id")
+  async getById(@Param("id") id: string): Promise<Task> {
+    return this.taskService.getById(id);
+  }
+
+  @Post()
+  @HttpCode(201)
+  async create(@Body() task: Task): Promise<Task> {
+    return this.taskService.create(task);
+  }
+
+  @Patch(":id")
+  async update(@Param("id") id: string, @Body() task: Task): Promise<void> {
+    return this.taskService.update({
+      id,
+      description: task.description,
+      completed: task.completed
+    });
+  }
+
+  @Delete(":id")
+  async delete(@Param("id") id: string): Promise<void> {
+    return this.taskService.delete(id);
+  }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,83 @@
-import { Injectable } from "@nestjs/common";
+import { v4 as uuidV4 } from "uuid";
+
+import { Injectable, HttpStatus, HttpException } from "@nestjs/common";
+
+import { Task } from "./tasks";
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [
+    {
+      id: "18b54eec-45e6-43f6-8cf5-55eaac191456",
+      description: "task 1",
+      completed: false
+    },
+    {
+      id: "77d3569b-90bd-4d19-b390-b55a5fbaa92f",
+      description: "task 2",
+      completed: false
+    },
+    {
+      id: "544d4941-9e15-435d-963d-d985863b3a6d",
+      description: "task 3",
+      completed: false
+    }
+  ];
+
+  async getAll(): Promise<Task[]> {
+    return this.tasks;
+  }
+
+  async getById(id: string): Promise<Task> {
+    const task = this.tasks.find((task) => {
+      return task.id === id;
+    });
+
+    if (!task) {
+      throw new HttpException("Task Not Found", HttpStatus.NOT_FOUND);
+    }
+
+    return task;
+  }
+
+  async create(task: Task): Promise<Task> {
+    const id = uuidV4();
+
+    this.tasks.push({
+      id,
+      description: task.description,
+      completed: task.completed
+    });
+
+    return {
+      id,
+      description: task.description,
+      completed: task.completed
+    };
+  }
+
+  async update(task: Task): Promise<void> {
+    const taskAlreadyExists = this.tasks.find((taskElement) => {
+      return taskElement.id === task.id;
+    });
+
+    if (!taskAlreadyExists) {
+      throw new HttpException("Task not exists", HttpStatus.BAD_REQUEST);
+    }
+
+    this.tasks.map((taskElement) => {
+      if (taskElement.id === task.id) {
+        taskElement.description = task.description;
+        taskElement.completed = task.completed;
+      }
+    });
+  }
+
+  async delete(id: string): Promise<void> {
+    const taskIndex = this.tasks.findIndex((task) => {
+      return task.id === id;
+    });
+
+    this.tasks.splice(taskIndex, 1);
+  }
+}
