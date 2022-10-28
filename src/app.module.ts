7090a4479a9a3e2b3765a27c71d8b52bba371fb0
Caio Vinícius <caio1525pereira@gmail.com>
2022-10-28 10:43:53 -0300

    feat: :wrench: Configuração inicial do mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { TasksModule } from "./tasks/tasks.module";
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGODB_CONNECTION_STRING),
+    TasksModule
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
